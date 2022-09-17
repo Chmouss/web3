a3d3e48d15da7cce7f420c97b14a12234d1eacbe
Chmouss <cyprien.lek@hotmail.com>
2022-09-17 19:30:16 +0200

    1.1-1.2 : import image

--- exercises/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
+import pasta from 'components/Header/pasta.jpg'
+
+console.log(pasta);
+
 const Header = (props) =>{
     return (
       <div>
+        <p>
+            <img src={pasta} alt="Pasta" />;
+        </p>
         <p>
           {props.course}
         </p>
