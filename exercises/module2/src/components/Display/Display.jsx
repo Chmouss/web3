f7f6cf79324265568e8e554d0bc391092e01b4d1
Chmouss <cyprien.lek@hotmail.com>
2022-09-23 12:21:39 +0200

    module 2 : init

---
+++ exercises/module2/src/components/Display/Display.jsx
@@ -0,0 +1,9 @@
+const Display = ({ counter }) => {
+    return (
+      <div>{counter}</div>
+    )
+  }
+
+export default Display
+
+//const Display = ({ counter }) => <div>{counter}</div> on peut mettre ça comme ça (tres raccourci)
