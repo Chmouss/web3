7a4e7cb4ff75a0520c1d3d03999a2775a671bbe8
Chmouss <cyprien.lek@hotmail.com>
2022-09-25 16:30:17 +0200

    module2 : localStorage

--- exercises/module2/src/components/App/App.js
+++ exercises/module2/src/components/App/App.js
@@ -1,23 +1,34 @@
 import Display from "components/Display/Display"
 import Button from "components/Button/Button"
+//import React from "react"
 
 import { useState } from "react"
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [ counter, setCounter ] = useState(JSON.parse(localStorage.getItem("counter")) ?? 0)
+  // ->    ?? if premiere cond null ou undefined, il prend la valeur a droite
 
   //const increaseByOne = () => setCounter(counter + 1)
   //const decreaseByOne = () => setCounter(counter - 1)
   //const setToZero = () => setCounter(0)
 
-  const changeCount = (delta) => setCounter(counter + delta)
+  const changeCount = (delta) => {
+    setCounter(counter + delta)
+    localStorage.setItem("counter", JSON.stringify(counter + delta))
+  }
 
   return (
     <div>
       <Display counter={counter}/>
       <Button
-        onClick={changeCount(counter)}
+        onClick={changeCount}
         text='plus'
+        value={1}
+      /> 
+      <Button
+        onClick={changeCount}
+        text='moins'
+        value={-1}
       /> 
     </div>
   )
