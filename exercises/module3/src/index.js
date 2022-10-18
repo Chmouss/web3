c39ef9e0123219ad7f1138f9debb7f582c0e3fb3
Chmouss <cyprien.lek@hotmail.com>
2022-10-18 17:21:52 +0200

    update

--- exercises/module3/src/index.js
+++ exercises/module3/src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom/client'
 
 import App from './components/App/App'
 
+//TODO sur chrome ajouter extension react pour voir components
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
